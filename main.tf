376dbca606936151ded59806980374a5e098fba9
Vladimir Mukhin <Vladimir_Mukhin@epam.com>
2021-08-25 23:32:03 -0400

    Add lambda function to connect to postgres

--- main.tf
+++ main.tf
@@ -44,7 +44,7 @@ resource "aws_security_group" "allow-postgres" {
     from_port   = 5432
     protocol    = "tcp"
     to_port     = 5432
-    cidr_blocks = [var.source_ip]
+    cidr_blocks = [var.source_ip, "10.0.0.0/16"]
   }
   egress {
     from_port   = 0
@@ -74,15 +74,94 @@ resource "aws_db_instance" "postgres" {
   storage_type            = "gp2"
   backup_retention_period = 7
   skip_final_snapshot     = true
-  publicly_accessible     = true
+  publicly_accessible     = false
 }
 
-provider "postgresql" {
-  host     = aws_db_instance.postgres.address
-  username = "root"
-  password = "tempunsecurepassword"
+resource "aws_iam_role" "sample" {
+  name = "sample"
+
+  inline_policy {
+    name = "lambda_vpc"
+
+    policy = jsonencode({
+      Version = "2012-10-17"
+      Statement = [
+        {
+          Action   = [
+            "ec2:CreateNetworkInterface",
+            "ec2:DescribeNetworkInterfaces",
+            "ec2:DeleteNetworkInterface"
+          ]
+          Effect   = "Allow"
+          Resource = "*"
+        },
+      ]
+    })
+  }
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": [
+          "lambda.amazonaws.com",
+          "edgelambda.amazonaws.com"
+        ]
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
 }
 
-resource "postgresql_database" "my_db1" {
-  name     = "my_db1"
+data "archive_file" "lambda_zip" {
+  type        = "zip"
+  source_dir = "lambda"
+  output_path = "lambda_function.zip"
+}
+
+resource "aws_security_group" "lambda" {
+  vpc_id      = module.vpc.vpc_id
+  name        = "lambda"
+  description = "lambda"
+  egress {
+    from_port   = 0
+    protocol    = "-1"
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+    self        = true #(Optional) Whether the security group itself will be added as a source to this egress rule.
+  }
+  tags = {
+    Name = "lambda"
+  }
+}
+
+resource "aws_lambda_function" "sample" {
+  filename         = "lambda_function.zip"
+  function_name    = "sample"
+  role             = aws_iam_role.sample.arn
+  handler          = "lambda_function.lambda_handler"
+  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
+  runtime          = "python3.8"
+  publish          = true
+
+  vpc_config {
+    subnet_ids         = module.vpc.public_subnets
+    security_group_ids = [aws_security_group.lambda.id]
+  }
+
+  environment {
+    variables = {
+      ENDPOINT   = aws_db_instance.postgres.address
+      PORT       = "5432"
+      DBUSER     = "root"
+      DBPASSWORD = "tempunsecurepassword"
+      DATABASE   = "postgres"
+    }
+  }
 }
